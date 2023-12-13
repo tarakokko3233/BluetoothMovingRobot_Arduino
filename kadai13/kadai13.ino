f44c851a394a41fd2657d0e186ca020ca758f34f
tarakokko3233 <tanahashist1230@outlook.jp>
2023-12-13 12:33:38 +0900

    演習12/13

--- kadai13/kadai13.ino
+++ kadai13/kadai13.ino
@@ -5,14 +5,47 @@
 #define DXL_ID_1  1
 #define DXL_ID_2  2
 
-#define SPEED_FOR_ID_1  400
-#define SPEED_FOR_ID_2  100
-
 DynamixelWorkbench dxl_wb;
+void performMovement() {
+  // モーターの速度を設定
+  dxl_wb.goalVelocity(DXL_ID_1, 400);
+  dxl_wb.goalVelocity(DXL_ID_2, -400);
+  Serial.println("go");
+  delay(1100);
+
+  // モーターを停止
+  dxl_wb.goalVelocity(DXL_ID_1, 0);
+  dxl_wb.goalVelocity(DXL_ID_2, 0);
+  delay(500);
+  Serial.println("stop");
+
+  // 右ホイールと左ホイールの速度を設定して回転
+  dxl_wb.goalVelocity(DXL_ID_1, 500);
+  dxl_wb.goalVelocity(DXL_ID_2, 0);
+  delay(1200);
+  Serial.println("turn");
+
+  // 再び停止
+  dxl_wb.goalVelocity(DXL_ID_1, 0);
+  dxl_wb.goalVelocity(DXL_ID_2, 0);
+  Serial.println("stop");
+   delay(500);
+
+  dxl_wb.goalVelocity(DXL_ID_1, 400);
+  dxl_wb.goalVelocity(DXL_ID_2, -400);
+  Serial.println("go");
+  delay(1100);
+
+  
+ dxl_wb.goalVelocity(DXL_ID_1, 0);
+  dxl_wb.goalVelocity(DXL_ID_2, 0);
+  Serial.println("stop");
+    delay(500);
+}
 
 void setup() {
   Serial.begin(BAUDRATE);
-  while(!Serial); // Wait for Opening Serial Monitor
+  //while(!Serial); // Wait for Opening Serial Monitor
 
   const char *log;
   bool result = false;
@@ -71,26 +104,13 @@ result = dxl_wb.setVelocityControlMode(DXL_ID_2, &log);
     Serial.println("Succeeded to set wheel mode for ID 2");
   }
 
+
+
+  // ロボットの動作を実行
+  performMovement();
+
 }
 
 void loop() {
-
-moveForward(100000000000000); // 10cm前進
-Serial.println("moved forward");
-delay(1000); // 小休止
-Serial.println("stopped");
-turn(10); // 90度回転
-Serial.println("turned");
-delay(1000); // 小休止
-Serial.println("stopped");
-moveForward(100000000000000); // 10cm前進
-Serial.println("moved forward");
-delay(10000); // 一時停止
-
-Serial.println("stopped");
-turn(10);
-Serial.println("turned");
-}
-
-
- 
+  
+} 
